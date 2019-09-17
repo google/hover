8db94a7fabe5e67b74d71331f788573b8042738c
Bruce Kim <bruce.kim@buzzvil.com>
2019-09-17 16:55:07 +0900

    Remove unused import

--- hover/src/main/java/io/mattcarroll/hover/ExitView.java
+++ hover/src/main/java/io/mattcarroll/hover/ExitView.java
@@ -25,7 +25,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.animation.PathInterpolatorCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
